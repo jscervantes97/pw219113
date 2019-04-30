b4afa3ddbff7fa87e500de2493f8ab5054908d33
Chuy <jscervantesmedina@gmail.com>
2019-04-30 13:58:24 -0600

    Listado de Clientes y Controlador

--- crud_node/src/rutas/index.js
+++ crud_node/src/rutas/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const rutas = express.Router();
+const customerController = require('../controllers/customerController');
 
+/*
 rutas.get("/",(req,res) => {
 	res.send("<marquee>hola mundo pero en node jajajaja </marquee>");
 });
-
+*/
+rutas.get('/',customerController.list);
+/*rutas.post('/add',customerController.save);
+rutas.get('/delete',customerController.delete);
+rutas.get('/update/:id',customerController.edit);
+rutas.post('/update/:id',customerController.update);
+*/
 module.exports = rutas;
