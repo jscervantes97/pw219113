cec00cbfb77b1472a6b481c977ae45f779ae9673
Chuy <jscervantesmedina@gmail.com>
2019-05-09 13:58:20 -0600

    2do Ejemplo VUEJS

--- vue_ejemplos/js/primervue.js
+++ vue_ejemplos/js/primervue.js
@@ -9,6 +9,31 @@ const app = new Vue({
 			{nombre:'Peras',cantidad: 80},
 			{nombre:'Fresas',cantidad: 110},
 			{nombre:'Papaya',cantidad: 10},
-		]
+		],
+		nuevaFruta: ''
+	},
+	methods:{
+		agregarFruta(){
+			//console.log("Diste Click");
+			if(this.nuevaFruta == ""){
+				alert("Faltan Campos por validar");
+				return ;
+			}
+			this.frutas.push(this.nuevaFruta);
+			this.frutas2.push({
+				nombre: this.nuevaFruta ,
+				cantidad : 0
+			});
+			this.nuevaFruta = '';
+		}
+	},
+	computed:{
+		sumarFrutas(){
+			this.total = 0 ;
+			for(fruta of this.frutas2){
+				this.total += fruta.cantidad;
+			}
+			return this.total; 
+		}
 	}
 });
